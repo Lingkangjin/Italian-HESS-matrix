d9e9a116b5acacefc5b5d848da50e4d59f3a5ebe
Lingkang <lingkang32@gmail.com>
2023-07-12 20:49:33 +0200

    Update ML_about_PV.py
    Tried:
    - standard scaler using scikitlearn
    - maybe power transformer is needed (so it is more gaussian wise)
    - DBSCAN can be applied and tried

--- ML_about_PV.py
+++ ML_about_PV.py
@@ -31,7 +31,8 @@ nature_colors = ['#4E79A7', '#F28E2B', '#E15759', '#76B7B2', '#59A14F', '#EDC948
 plt.style.use(["science", "nature"])
 
 # plt.rcParams["font.family"] = "Times New Roman"
-
+plt.rcParams.update({"figure.dpi": "200"})
+plt.rcParams["figure.constrained_layout.use"] = True
 
 
 #%%
@@ -60,6 +61,125 @@ for i,j in enumerate(df["Days"].unique()):
  
 df_grouped.index=np.arange(1,24+1,1)   
 df_grouped=df_grouped.T
+
+#%% scikit learn
+
+from sklearn.cluster import KMeans
+
+scaler=StandardScaler()
+X = scaler.fit_transform(df_grouped)
+
+# cluster = KMeans(n_clusters=8)
+# cluster.fit(X)
+# # inert.append(cluster.inertia_)
+
+
+
+# a=scaler.inverse_transform(cluster.cluster_centers_) 
+
+
+
+wcss=[]
+Silhoutte = []
+
+
+
+for i in tqdm(range(2,20)):
+    cluster = KMeans(n_clusters=i,init = 'k-means++', max_iter=300, n_init=10, random_state=0)
+
+
+    cluster.fit(X)
+    wcss_iter = cluster.inertia_
+    wcss.append(wcss_iter)
+    labels = cluster.fit_predict(X)
+    
+    Silhoutte.append(silhouette_score(X, labels))
+    
+    
+# import os
+# os.environ["OMP_NUM_THREADS"] = '2'
+    
+#%% standard scaler
+number_clusters = range(2,20)
+
+
+kl = KneeLocator(number_clusters,wcss, curve="convex", direction="decreasing")
+
+fig1,ax=plt.subplots(nrows=2, ncols=1,sharex=True)
+ax[0].plot(number_clusters,wcss,marker=".",label="Inertia",color=nature_colors[0])
+ax[0].axvline(x=kl.knee,color="gray",label=f"Inertia at {kl.knee} cl")
+
+ax[0].set_ylabel('Inertia')
+ax[0].legend()
+# ax[0].set_xlabel('Number of clusters')
+# ax[0].grid()
+ax[0].tick_params(which='minor', bottom=False, top=False, right=False, left=False)
+
+
+
+
+ax[1].plot(number_clusters,Silhoutte,marker="s",label="Silhoutte",color=nature_colors[1])
+ax[1].legend()
+ax[1].set_xlabel('Number of clusters')
+# ax[1].grid()
+ax[1].tick_params(which='minor', bottom=False, top=False, right=False, left=False)
+ax[1].legend()
+ax[1].set_ylabel("Silhoutte")
+
+plt.xticks(range(2,50))
+
+
+plt.suptitle('scikit-learn-The Elbow test: determining K')    
+
+
+#%%
+cluster = KMeans(n_clusters=kl.knee,random_state=0)
+
+y_pred = cluster.fit_predict(X)
+
+X_back=scaler.inverse_transform(X)
+X_centers=scaler.inverse_transform(cluster.cluster_centers_)
+
+plt.figure(888)
+for yi in range(kl.knee):
+    plt.subplot(3, (kl.knee//3)+1, yi + 1)
+    for xx in X_back[y_pred == yi]:
+        plt.plot(xx.ravel(), "k-", alpha=.2)
+    plt.plot(X_centers[yi].ravel(), "r-")
+    plt.text(0.45, 0.85,'Cluster %d' % (yi + 1),
+             transform=plt.gca().transAxes,fontsize=6)
+    plt.xlim(0, 24)
+   
+    if yi == 1:
+        plt.title("Euclidean $k$-means using scikit-learn \n and inversed back")
+#%%
+
+labels = cluster.fit_predict(X)
+
+sil_media = silhouette_score(X, labels)
+samp = silhouette_samples(X, labels)
+
+for i in labels:
+    print(f"{np.mean(samp[labels == i] > sil_media)} \n")
+
+np.unique(labels, return_counts=True) # number of elements in each clusters
+
+
+from scipy.stats import skew, skewtest
+np.abs(skew(X))
+
+
+
+
+
+
+
+
+
+
+
+    
+
 #%%
 
 X_train=TimeSeriesScalerMeanVariance().fit_transform(df_grouped) #Scaler for time series. Scales time series so that their mean (resp. standard deviation) in each dimension is mu (resp. std).
@@ -69,6 +189,8 @@ X_train=TimeSeriesScalerMeanVariance().fit_transform(df_grouped) #Scaler for tim
 X_reshaped = X_train.reshape(X_train.shape[0], -1) #reshaping for easy to manage
 #%%
 
+
+
 # X_reshaped =df_grouped.to_numpy()
 
 
@@ -77,16 +199,19 @@ km = TimeSeriesKMeans(n_clusters=3, verbose=True, random_state=seed)
 y_pred = km.fit_predict(X_reshaped)
 
 
-plt.figure()
-for yi in range(3):
-    plt.subplot(3, 3, yi + 1)
-    for xx in X_reshaped[y_pred == yi]:
-        plt.plot(xx.ravel(), "k-", alpha=.2)
-    plt.plot(km.cluster_centers_[yi].ravel(), "r-")
-    plt.xlim(0, 24)
+# plt.figure()
+# for yi in range(3):
+#     plt.subplot(3, 3, yi + 1)
+#     for xx in X_reshaped[y_pred == yi]:
+#         plt.plot(xx.ravel(), "k-", alpha=.2)
+#     plt.plot(km.cluster_centers_[yi].ravel(), "r-")
+#     plt.xlim(0, 24)
    
-    if yi == 1:
-        plt.title("Euclidean $k$-means")
+#     if yi == 1:
+#         plt.title("Euclidean $k$-means")
+        
+        
+        
 
 #%%
 wcss=[]
@@ -106,8 +231,7 @@ for i in tqdm(range(2,20)):
     
 
 #%%
-plt.rcParams.update({"figure.dpi": "200"})
-plt.rcParams["figure.constrained_layout.use"] = True
+
 
 number_clusters = range(2,20)
 
@@ -116,7 +240,7 @@ kl = KneeLocator(number_clusters,wcss, curve="convex", direction="decreasing")
 
 
 
-fig,ax=plt.subplots(nrows=2, ncols=1,sharex=True)
+fig2,ax=plt.subplots(nrows=2, ncols=1,sharex=True)
 ax[0].plot(number_clusters,wcss,marker=".",label="Inertia",color=nature_colors[0])
 ax[0].axvline(x=kl.knee,color="gray",label=f"Inertia at {kl.knee} cl")
 ax[0].set_ylabel('Inertia')
@@ -148,6 +272,8 @@ seed=0 #random state
 km = TimeSeriesKMeans(n_clusters=k, verbose=True, random_state=seed)
 y_pred = km.fit_predict(X_reshaped)
 
+plt.figure(999)
+
 for yi in range(kl.knee):
     plt.subplot(3, (kl.knee//3)+1, yi + 1)
     for xx in X_reshaped[y_pred == yi]:
@@ -158,7 +284,7 @@ for yi in range(kl.knee):
     plt.xlim(0, 24)
    
     if yi == 1:
-        plt.title("Euclidean $k$-means")
+        plt.title("Euclidean $k$-means con tslearn")
         
         
 #%% transforming back
